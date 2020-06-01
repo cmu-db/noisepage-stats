d0578e0336d3386d84f02e9915dbd7aaf4b720eb
Daniel Biales <bialesdaniel@gmail.com>
2020-06-01 11:51:14 -0400

    comment out the admin

--- performance-storage-service/pss_project/urls.py
+++ performance-storage-service/pss_project/urls.py
@@ -7,6 +7,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('pss_project.api.urls'))
 ]
