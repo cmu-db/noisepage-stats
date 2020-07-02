a233d897b8bac78402799d7c956776fd4a6c9338
Ricthofen <jazzplato@gmail.com>
2020-07-01 23:49:53 -0400

    update static url

--- performance-storage-service/pss_project/settings/base.py
+++ performance-storage-service/pss_project/settings/base.py
@@ -110,4 +110,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/performance-results/static/'
+STATIC_URL = '/static/pss/'
