e70419f6f3c438e83503ff9878fdfb372bc09ec9
Ricthofen <jazzplato@gmail.com>
2020-07-17 23:46:05 -0400

    update secret name

--- deployments/scripts/make_secrets.sh
+++ deployments/scripts/make_secrets.sh
@@ -3,8 +3,8 @@ ENV=$1
 DIR_BASE="$(dirname $(realpath $0))"
 DIR_ENV="$DIR_BASE/$ENV"
 NAMESPACE="performance"
-SECRET_NAME="pss-db-secrets-$ENV"
-HELP="Usage: ./make_secrets_pss_db.sh ENV"
+SECRET_NAME="secrets-$ENV"
+HELP="Usage: ./make_secrets.sh ENV"
 
 if [ $# -lt 1 ]; then
     echo $HELP
@@ -17,5 +17,8 @@ if [ ! -d $DIR_ENV ]; then
 fi
 
 kubectl create secret generic $SECRET_NAME -n $NAMESPACE \
-    --from-file=$ENV/pss_db_user \
-    --from-file=$ENV/pss_db_password
+    --from-file=$DIR_ENV/pss_db_user \
+    --from-file=$DIR_ENV/pss_db_password \
+    --from-file=$DIR_ENV/gf_admin_password \
+    --from-file=$DIR_ENV/gf_auth_github_client_id \
+    --from-file=$DIR_ENV/gf_auth_github_client_secret
