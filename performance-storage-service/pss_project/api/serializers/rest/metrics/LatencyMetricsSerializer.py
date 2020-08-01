6e869a553ed6454c4a9622f3b003e00c23db3304
Daniel Biales <bialesdaniel@gmail.com>
2020-07-31 22:39:04 -0400

    GEN-327 update the latency metrics so that they are Decimals not Integers

--- performance-storage-service/pss_project/api/serializers/rest/metrics/LatencyMetricsSerializer.py
+++ performance-storage-service/pss_project/api/serializers/rest/metrics/LatencyMetricsSerializer.py
@@ -1,17 +1,17 @@
-from rest_framework.serializers import Serializer, IntegerField
+from rest_framework.serializers import Serializer, DecimalField
 from pss_project.api.models.rest.metrics.LatencyMetrics import LatencyMetrics
 
 
 class LatencyMetricsSerializer(Serializer):
-    l_25 = IntegerField(required=False)
-    l_75 = IntegerField(required=False)
-    l_90 = IntegerField(required=False)
-    l_95 = IntegerField(required=False)
-    l_99 = IntegerField(required=False)
-    avg = IntegerField(required=False)
-    median = IntegerField(required=False)
-    max = IntegerField(required=False)
-    min = IntegerField(required=False)
+    l_25 = DecimalField(max_digits=10, decimal_places=4, coerce_to_string=False)
+    l_75 = DecimalField(max_digits=10, decimal_places=4, coerce_to_string=False)
+    l_90 = DecimalField(max_digits=10, decimal_places=4, coerce_to_string=False)
+    l_95 = DecimalField(max_digits=10, decimal_places=4, coerce_to_string=False)
+    l_99 = DecimalField(max_digits=10, decimal_places=4, coerce_to_string=False)
+    avg = DecimalField(max_digits=10, decimal_places=4, coerce_to_string=False)
+    median = DecimalField(max_digits=10, decimal_places=4, coerce_to_string=False)
+    max = DecimalField(max_digits=10, decimal_places=4, coerce_to_string=False)
+    min = DecimalField(max_digits=10, decimal_places=4, coerce_to_string=False)
 
     def create(self, validated_data):
         return LatencyMetrics(**validated_data)
