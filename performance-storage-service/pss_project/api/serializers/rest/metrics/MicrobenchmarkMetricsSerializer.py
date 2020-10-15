6cacdb817792f25c9c86ae47998bd42c88ac78d8
Daniel Biales <bialesdaniel@gmail.com>
2020-10-15 02:59:32 -0400

    Updating required fields

--- performance-storage-service/pss_project/api/serializers/rest/metrics/MicrobenchmarkMetricsSerializer.py
+++ performance-storage-service/pss_project/api/serializers/rest/metrics/MicrobenchmarkMetricsSerializer.py
@@ -6,11 +6,11 @@ from pss_project.api.constants import MICROBENCHMARK_STATUS_CHOICES
 class MicrobenchmarkMetricsSerializer(Serializer):
     # Fields
     throughput = DecimalField(max_digits=24, decimal_places=15, coerce_to_string=False)
+    tolerance = IntegerField()
+    iterations = IntegerField()
     status = ChoiceField(required=False, choices=MICROBENCHMARK_STATUS_CHOICES)
     ref_throughput = DecimalField(required=False, max_digits=24, decimal_places=15, coerce_to_string=False)
     stdev_throughput = DecimalField(required=False, max_digits=24, decimal_places=15, coerce_to_string=False)
-    tolerance = IntegerField()
-    iterations = IntegerField()
 
     def create(self, validated_data):
         return MicrobenchmarkMetrics(**validated_data)
