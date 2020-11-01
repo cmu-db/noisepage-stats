dd676556686dd23886cfaaaa16923b149a6abac8
Daniel Biales <bialesdaniel@gmail.com>
2020-11-01 03:30:04 -0500

    Updated some python dependencies. Also, updated the git bot to be much closer to the working version. It now looks for a status update that signals the completion of the CI.

--- performance-storage-service/pss_project/api/views/git_events.py
+++ performance-storage-service/pss_project/api/views/git_events.py
@@ -1,130 +1,65 @@
-from rest_framework.viewsets import ViewSet 
-from rest_framework.response import Response 
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 
-from github3 import GitHub
-import time
-from jwt import JWT, jwk_from_pem
-import requests
+from pss_project.github_integration.NoisePageRepoClient import NoisePageRepoClient
 
 # Secrets that will eventuall have to move
-GITHUB_PRIVATE_KEY= """"""
+GITHUB_PRIVATE_KEY = """"""
 GITHUB_APP_IDENTIFIER = 86997
-GITHUB_WEBHOOK_SECRET = 'incrudibles'
-ALLOWED_EVENTS = ['pull_request', 'check_suite','check_run']
+ALLOWED_EVENTS = ['pull_request', 'state']
+CI_STATUS_CONTEXT = 'continuous-integration/jenkins/pr-merge'
 
-class NoisePageRepoClient():
-    def __init__(self, private_key, app_id):
-        self.git_client = GitHub()
-        self.git_client.login_as_app(private_key_pem=str.encode(GITHUB_PRIVATE_KEY),app_id=GITHUB_APP_IDENTIFIER)
-        self.owner = 'cmu-mse-cmudb'
-        self.repo = 'terrier'
-        self.noisepage_repo_client = self.git_client.app_installation_for_repository(self.owner,self.repo)
 
-        #self.git_client.login_as_app_installation(private_key_pem=str.encode(GITHUB_PRIVATE_KEY),app_id=GITHUB_APP_IDENTIFIER,installation_id=self.noisepage_repo_client.id)
-
-    def is_valid_installation_id(self, id):
-        return id == self.noisepage_repo_client.id
-
-    def _get_jwt(self):
-        """
-        This is needed to retrieve the installation access token (for debugging). 
-        
-        Useful for debugging purposes.  Must call .decode() on returned object to get string.
-        """
-        jwt = JWT()
-        now = int(time.time())
-        payload = {
-            "iat": now,
-            "exp": now + (60),
-            "iss": GITHUB_APP_IDENTIFIER
-        }
-        private_key = jwk_from_pem(str.encode(GITHUB_PRIVATE_KEY,'utf-8'))
-        return jwt.encode(payload, private_key, alg='RS256')
-
-    def _get_installation_access_token(self):
-        "Get the installation access token for debugging."
-        headers = {'Authorization': f'Bearer {self._get_jwt()}',
-                'Accept': 'application/vnd.github.machine-man-preview+json'}
-        
-        response = requests.post(url=self.noisepage_repo_client.access_tokens_url, headers=headers)
-        response.raise_for_status()
-        return response.json()['token']
-
-    def create_check_run(self,sha):
-        token = self._get_installation_access_token()
-        headers = {
-            'Authorization': f'Bearer {token}',
-            'Content-Type': 'application/json',
-            "Accept": "application/vnd.github.v3+json"
-        }
-        url = f"https://api.github.com/repos/{self.owner}/{self.repo}/check-runs"
-        body = {
-            "name":"performance-cop",
-            "head_sha": f"{sha}"
-        }
-        response = requests.post(url=url,json=body,headers=headers)
-        response.raise_for_status()
-
-    def update_check_run(self, check_run_id):
-        token = self._get_installation_access_token()
-        headers = {
-            'Authorization': f'Bearer {token}',
-            'Content-Type': 'application/json',
-            "Accept": "application/vnd.github.v3+json"
-        }
-        url = f"https://api.github.com/repos/{self.owner}/{self.repo}/check-runs/{check_run_id}"
-        body = {
-            "name":"performance-cop",
-            "status": "completed",
-            "conclusion":"success",
-            "output":{
-                "title":"You are a performance hero!",
-                "summary":"This PR actually improved the performance. Nice Job!",
-                "text":"You improved performance of test x by 1%\n you improved b by 2% \n c by 3% yada yada yada"
-            }
-        }
-        response = requests.patch(url=url,json=body,headers=headers)
-        response.raise_for_status()
-    
-
-#TODO: add github3.py to requirements.txt
+# TODO: add github3.py to requirements.txt
 class GitEventsViewSet(ViewSet):
 
-    def create(self,request):
+    def create(self, request):
         payload = JSONParser().parse(request)
-
         if not any([event in payload for event in ALLOWED_EVENTS]):
-            return Response({"message":"This app is only designed to handle check_suite events"},status=HTTP_400_BAD_REQUEST)
-        
-        repo_client = NoisePageRepoClient(private_key=GITHUB_PRIVATE_KEY, app_id=GITHUB_APP_IDENTIFIER)
-        if(not repo_client.is_valid_installation_id(payload.get('installation',{}).get('id'))):
-            return Response({"message":"This app only works with the NoisePage repo"},status=HTTP_400_BAD_REQUEST)
+            return Response({"message": "This app is only designed to handle pull_request and status events"},
+                            status=HTTP_400_BAD_REQUEST)
 
-        if 'check_suite' in payload:
-            handle_check_suite_event(repo_client, payload)
+        repo_client = NoisePageRepoClient(private_key=GITHUB_PRIVATE_KEY, app_id=GITHUB_APP_IDENTIFIER)
+        if(not repo_client.is_valid_installation_id(payload.get('installation', {}).get('id'))):
+            return Response({"message": "This app only works with the NoisePage repo"}, status=HTTP_400_BAD_REQUEST)
+        
         if 'pull_request' in payload:
             handle_pull_request_event(repo_client, payload)
-        if 'check_run' in payload:
-            handle_check_run_event(repo_client, payload)
+        if 'state' in payload:
+            handle_status_event(repo_client, payload)
 
-        
         return Response(status=HTTP_200_OK)
 
 
-def handle_check_suite_event(repo_client, payload):
-    try:
-        if payload.get('action')!= 'completed':
-            repo_client.create_check_run(payload['check_suite'].get('head_sha'))
-    except Exception as err:
-        print(err)
 
-def handle_check_run_event(repo_client, payload):
-    # TODO: This is very temporary code to allow me to test this better
-    if payload.get('action')!= 'completed':
-        repo_client.update_check_run(payload['check_run'].get('id'))
+def handle_status_event(repo_client, payload):
+    commit_sha = payload.get('commit',{}).get('sha')
+    status_response = repo_client.get_commit_status(commit_sha)
+    if is_status_ci_complete(status_response):
+        check_run = repo_client.get_commit_check_run_for_app(commit_sha, GITHUB_APP_IDENTIFIER)
+        repo_client.update_check_run(check_run.get('id'), performance_check_result())
+
+def performance_check_result():
+    # TODO: This function will parse the results of the performance
+    # comparison
+    return {
+        "name": "performance-cop",
+        "status": "completed",
+        "conclusion": "success",
+        "output": {
+            "title": "You are a performance hero!",
+            "summary": "This PR actually improved the performance. Nice Job!",
+            "text": "You improved performance of test x by 1%\n you improved b by 2% \n c by 3% yada yada yada"
+        }
+    }
+
+
+def is_status_ci_complete(status_reponse):
+    if status_reponse.get('state') != 'success': return False 
+    return any([status.get('context') == CI_STATUS_CONTEXT for status in status_reponse.get('statuses',[])])
 
 def handle_pull_request_event(repo_client, payload):
     # TODO: probably want some logic for labels
-    repo_client.create_check_run(payload['pull_request'].get('head',{}).get('sha'))
+    repo_client.create_check_run(payload['pull_request'].get('head', {}).get('sha'))
---
+++ performance-storage-service/pss_project/github_integration/NoisePageRepoClient.py
@@ -0,0 +1,109 @@
+from github3 import GitHub
+import time
+from jwt import JWT, jwk_from_pem
+import requests
+
+OWNER = 'cmu-mse-cmudb'
+REPO = 'terrier'
+GITHUB_URL = 'https://api.github.com/'
+
+
+class NoisePageRepoClient():
+    def __init__(self, private_key, app_id):
+        """ Connect to github and create a Github client and a client specific
+        to the Github app installation"""
+        self.private_key = private_key
+        self.owner = OWNER
+        self.repo = REPO
+
+        self.git_client = GitHub()
+        self.git_client.login_as_app(private_key_pem=str.encode(private_key), app_id=app_id)
+        self.noisepage_repo_client = self.git_client.app_installation_for_repository(self.owner, self.repo)
+        self.access_token = {"token":None, "exp":0}
+
+    def is_valid_installation_id(self, id):
+        """Check whether an installation ID is the NoisePage installation
+        This will prevent other Github users from using the app"""
+        return id == self.noisepage_repo_client.id
+
+    def _get_jwt(self):
+        """ This creates a JWT that can be used to retrieve an authentication
+        token for the Github app."""
+        jwt = JWT()
+        now = int(time.time())
+        payload = {
+            "iat": now,
+            "exp": now + (60),
+            "iss": self.noisepage_repo_client.app_id
+        }
+        private_key = jwk_from_pem(str.encode(self.private_key))
+        return jwt.encode(payload, private_key, alg='RS256'), now + (60)
+
+    def _get_installation_access_token(self):
+        """ Get the installation access token for making API calls not
+        supported by github3.py. Only get a new token if the current one has
+        expired. """
+        if time.time() > self.access_token.get('exp'):
+            jwt,exp = self._get_jwt()
+            headers = {'Authorization': f'Bearer {jwt}',
+                    'Accept': 'application/vnd.github.v3+json'}
+            response = requests.post(url=self.noisepage_repo_client.access_tokens_url, headers=headers)
+            response.raise_for_status()
+            self.access_token = {"token":response.json().get('token'), "exp":exp}
+        return self.access_token.get('token')
+
+    def create_check_run(self, sha):
+        """ Create a check run for the performance cop """
+        token = self._get_installation_access_token()
+        headers = {
+            'Authorization': f'Bearer {token}',
+            "Accept": "application/vnd.github.v3+json"
+        }
+        url = f"{GITHUB_URL}repos/{self.owner}/{self.repo}/check-runs"
+        body = {
+            "name": "performance-cop",
+            "head_sha": f"{sha}"
+        }
+        response = requests.post(url=url, json=body, headers=headers)
+        response.raise_for_status()
+        return response.json()
+
+    def update_check_run(self, check_run_id, update_body):
+        """ Update a check run to mark it as complete """
+        token = self._get_installation_access_token()
+        headers = {
+            'Authorization': f'Bearer {token}',
+            "Accept": "application/vnd.github.v3+json"
+        }
+        url = f"{GITHUB_URL}repos/{self.owner}/{self.repo}/check-runs/{check_run_id}"
+        response = requests.patch(url=url, json=update_body, headers=headers)
+        response.raise_for_status()
+        return response.json()
+
+    def get_commit_status(self, commit_sha):
+        """ Get the status of a commit """
+        token = self._get_installation_access_token()
+        headers = {
+            'Authorization': f'Bearer {token}',
+            "Accept": "application/vnd.github.v3+json"
+        }
+        url = f"{GITHUB_URL}repos/{self.owner}/{self.repo}/commits/{commit_sha}/status"
+        response = requests.get(url=url, headers=headers)
+        response.raise_for_status()
+        return response.json()
+
+    def get_commit_check_run_for_app(self, commit_sha, app_id):
+        """ Get the check runs for a commit """
+        token = self._get_installation_access_token()
+        headers = {
+            'Authorization': f'Bearer {token}',
+            "Accept": "application/vnd.github.v3+json"
+        }
+        url = f"{GITHUB_URL}repos/{self.owner}/{self.repo}/commits/{commit_sha}/check-runs"
+        response = requests.get(url=url, headers=headers)
+        response.raise_for_status()
+        check_runs = response.json().get('check_runs')
+        for run in check_runs:
+            if run.get('app',{}).get('id'):
+                return run
+        return {}
