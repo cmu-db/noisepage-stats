6d048e88ae7a01e78c59c9abcaca6610f6d2c59c
Ricthofen <jazzplato@gmail.com>
2020-11-11 22:46:58 -0500

    fixed import issue and missing kwargs

--- performance-storage-service/pss_project/api/serializers/rest/metrics/MemoryMetricsSerializer.py
+++ performance-storage-service/pss_project/api/serializers/rest/metrics/MemoryMetricsSerializer.py
@@ -1,17 +1,17 @@
-from rest_framework.serializers import Serializer, DecimalField
+from rest_framework.serializers import Serializer, DecimalField, IntegerField
 from pss_project.api.models.rest.metrics.MemoryMetrics import MemoryMetrics, MemoryItemSummary
 
 
 class MemoryItemSummarySerializer(Serializer):
-    avg = DecimalField(decimal_places=4, coerce_to_string=False)
+    avg = DecimalField(max_digits=100, decimal_places=4, coerce_to_string=False)
 
     def create(self, validated_data):
         return MemoryItemSummarySerializer(**validated_data)
 
 
 class MemoryMetricsSerializer(Serializer):
-    rss = IntegerField()
-    vms = IntegerField()
+    rss = IntegerField(min_value=0)
+    vms = IntegerField(min_value=0)
 
     def create(self, validated_data):
         return MemoryMetrics(**validated_data)
