e7a54e70b2bb5c6c28eb2bf366f645bee401d928
Daniel Biales <bialesdaniel@gmail.com>
2020-11-21 23:13:42 -0500

    Added logging

--- performance-storage-service/pss_project/api/views/git_events.py
+++ performance-storage-service/pss_project/api/views/git_events.py
@@ -9,22 +9,29 @@ from pss_project.api.constants import (GITHUB_APP_IDENTIFIER, ALLOWED_EVENTS, CI
                                                     WEBHOOK_SECRET, GITHUB_WEBHOOK_HASH_HEADER, GITHUB_PRIVATE_KEY,
                                                     PERFORMANCE_COP_CHECK_NAME)
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 class GitEventsViewSet(ViewSet):
 
     def create(self, request):
         """ This is the endpoint that Github events are posted to """
         if not is_valid_github_webhook_hash(request.META.get(GITHUB_WEBHOOK_HASH_HEADER), request.body):
             return Response({"message":"Invalid request hash. Only Github may call this endpoint."},status=HTTP_403_FORBIDDEN)
-        
+        logger.debug('Valid webhook hash')
+
         payload = JSONParser().parse(request)
         event = request.META.get('HTTP_X_GITHUB_EVENT')
         
+        logger.debug(f'Incoming {event} event')
         if not any([valid_event == event for valid_event in ALLOWED_EVENTS]):
             return Response({"message": f"This app is only designed to handle {ALLOWED_EVENTS} events"},
                             status=HTTP_400_BAD_REQUEST)
 
         try:
             repo_client = NoisePageRepoClient(private_key=GITHUB_PRIVATE_KEY, app_id=GITHUB_APP_IDENTIFIER)
+            logger.debug('Authenticated with Github repo')
 
             if(not repo_client.is_valid_installation_id(payload.get('installation', {}).get('id'))):
                 return Response({"message": "This app only works with the NoisePage repo"}, status=HTTP_400_BAD_REQUEST)
@@ -42,13 +49,8 @@ class GitEventsViewSet(ViewSet):
 def is_valid_github_webhook_hash(hash_header, req_body):
     """ Check that the has passed with the request is valid based on the
     webhook secret and the request body """
-    print(hash_header)
-    print(WEBHOOK_SECRET)
-    print(WEBHOOK_SECRET.strip())
     alg, req_hash = hash_header.split('=',1)
     valid_hash = hmac.new(str.encode(WEBHOOK_SECRET),req_body, alg)
-    print('intentional gap')
-    print(valid_hash)
     return hmac.compare_digest(req_hash, valid_hash.hexdigest())
 
 def handle_pull_request_event(repo_client, payload):
@@ -59,8 +61,10 @@ def handle_pull_request_event(repo_client, payload):
     if payload['pull_request'].get('user',{}).get('login') == 'bialesdaniel': 
         commit_sha = payload['pull_request'].get('head', {}).get('sha')
         if should_initialize_check_run(payload.get('action')):
-            initialize_check_run(commit_sha)
+            logger.debug('pull request reinitialized check')
+            initialize_check_run(repo_client, commit_sha)
         if payload.get('action') == 'closed':
+            logger.debug('pull request was closed')
             branch = payload['pull_request'].get('head',{}).get('ref')
             cleanup_check_run(branch)
 
@@ -68,11 +72,12 @@ def should_initialize_check_run(pull_request_action):
     """ Determine if the pull request event should initialize a check run """
     return any(pull_request_action == action for action in ['synchronize','opened','reopened'])
 
-def initialize_check_run(sha):
+def initialize_check_run(repo_client, sha):
     """ Create the initial performance cop check run. The run starts in the
     queued state """
     create_body = create_initial_check_run(sha)
     repo_client.create_check_run(create_body)
+    logger.debug('Initialized check run')
 
 def create_initial_check_run(sha):
     return {
@@ -89,7 +94,7 @@ def cleanup_check_run(branch):
     #TODO: here we will delete performance results for the PR branch from the DB
     # I also need to check what Jenkins stores in the git_branch column cause it
     # might not be the same as the branch in the event. 
-    pass
+    logger.debug('Cleaning up check run')
 
 
 def handle_status_event(repo_client, payload):
@@ -99,30 +104,34 @@ def handle_status_event(repo_client, payload):
     update the check run based on the results of the comparison """
     commit_sha = payload.get('commit',{}).get('sha')
     status_response = repo_client.get_commit_status(commit_sha)
-    if is_ci_complete(status_response):
+    if is_ci_complete(repo_client, status_response):
+        logger.debug('Status update indicated CI completed')
         complete_check_run(commit_sha)
-    elif status.get('context') == CI_STATUS_CONTEXT:
-        initialize_check_run_if_missing(commit_sha)
-
-def complete_check_run(commit_sha):
-    """ Update the check run with a complete status based on the performance
-    results. If the check run does not exist do nothing """
-    check_run = repo_client.get_commit_check_run_for_app(commit_sha, GITHUB_APP_IDENTIFIER)
-    if check_run: 
-        repo_client.update_check_run(check_run.get('id'), performance_check_result())
-
-def initialize_check_run_if_missing(commit_sha):
-    """ Check to see if the check run was already created. If it was not then
-    initialize the check run """
-    check_run = repo_client.get_commit_check_run_for_app(commit_sha, GITHUB_APP_IDENTIFIER)
-    if not check_run:
-        initialize_check_run(commit_sha)
+    elif status_response.get('context') == CI_STATUS_CONTEXT:
+        logger.debug('Status update indicated CI started')
+        initialize_check_run_if_missing(repo_client, commit_sha)
 
 def is_ci_complete(status_reponse):
     """ Check whether a status update indicates that the Jenkins pipeline
     is complete. This is based on the state and the context of the status """
     if status_reponse.get('state') != 'success': return False 
     return any([status.get('context') == CI_STATUS_CONTEXT for status in status_reponse.get('statuses',[])])
+
+def complete_check_run(repo_client, commit_sha):
+    """ Update the check run with a complete status based on the performance
+    results. If the check run does not exist do nothing """
+    check_run = repo_client.get_commit_check_run_for_app(commit_sha, GITHUB_APP_IDENTIFIER)
+    if check_run: 
+        repo_client.update_check_run(check_run.get('id'), performance_check_result())
+        logger.debug('Check run updated with performance results')
+
+def initialize_check_run_if_missing(repo_client, commit_sha):
+    """ Check to see if the check run was already created. If it was not then
+    initialize the check run """
+    check_run = repo_client.get_commit_check_run_for_app(commit_sha, GITHUB_APP_IDENTIFIER)
+    if not check_run:
+        initialize_check_run(commit_sha)
+        logger.debug('Check run did not exist. It has been initialized')
 
 def performance_check_result():
     # TODO: This function will parse the results of the performance
